3d3302bdcfe694ea33b2d3ebed1af0886c206a27
hemangjdave <hemangjdave@gmail.com>
2020-06-05 01:02:23 +0530

    Added one new test case to show sorting process.

--- src/main/java/com/techrevolution/sort/Main.java
+++ src/main/java/com/techrevolution/sort/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         InsertionSort sort = new InsertionSort();
-        int array[] = {82, 42, 23, 28, 92, 6, 52, 28, 88, 17};
+        int[] array = {82, 42, 23, 28, 92, 6, 52, 28, 88, 17};
 
-        int array2[] = {25 , 66 , 56 , 37 , 39 , 82 , 34 , 32 , 48 , 46};
+        int[] array2 = {25 , 66 , 56 , 37 , 39 , 82 , 34 , 32 , 48 , 46};
 
         sort.arrangeArrayInAscOrder(array);
 
@@ -18,5 +18,10 @@ public class Main {
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(array2));
 
+
+        int[] array3 = {9 , 8 , 7 , 6 , 5 , 4 , 3 , 2 , 1};
+        sort.arrangeArrayInAscOrder(array3);
+
+        System.out.println(Arrays.toString(array3));
     }
 }
--- src/test/java/com/techrevolution/sort/InsertionSortShould.java
+++ src/test/java/com/techrevolution/sort/InsertionSortShould.java
@@ -32,4 +32,15 @@ public class InsertionSortShould {
         assertEquals(Arrays.toString(expected), Arrays.toString(array));
     }
 
+    @Test
+    public void sortArray3() {
+        int[] array = {9 , 8 , 7 , 6 , 5 , 4 , 3 , 2 , 1};
+
+        sort.arrangeArrayInAscOrder(array);
+
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        assertEquals(Arrays.toString(expected), Arrays.toString(array));
+    }
+
 }
